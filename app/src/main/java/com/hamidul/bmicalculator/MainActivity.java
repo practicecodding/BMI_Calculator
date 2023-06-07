e31e6a1269d8c010d98dee2f0fb9e2bfa564cfe3
user <practicecoddin@gmail.com>
2023-06-07 07:36:39 +0600

    S.M.Hamidul

--- app/src/main/java/com/hamidul/bmicalculator/MainActivity.java
+++ app/src/main/java/com/hamidul/bmicalculator/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView check;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
