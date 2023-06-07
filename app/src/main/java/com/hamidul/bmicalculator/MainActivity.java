8d6380bdaeb697f384840dffb07778937b6ae0ac
user <practicecoddin@gmail.com>
2023-06-07 06:33:40 +0600

    BMI Calculator

---
+++ app/src/main/java/com/hamidul/bmicalculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hamidul.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
