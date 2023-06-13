fc9b6fce3d5c5400c5745b48c34de9b664c4b502
user <practicecoddin@gmail.com>
2023-06-13 07:34:21 +0600

    S.M.Hamidul

--- app/src/main/java/com/hamidul/bmicalculator/MainActivity.java
+++ app/src/main/java/com/hamidul/bmicalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hamidul.bmicalculator;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
-
 
 
 
